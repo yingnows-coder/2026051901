09156e6758f19ab22505eeeee1f93c345a9ba019
yingnows-coder <yingnows@gmail.com>
2026-05-19 19:21:06 +0800

    Update app.py

--- app.py
+++ app.py
@@ -9,9 +9,9 @@ col_left, col_center, col_right = st.columns([1, 2, 1.2], gap="large")
 with col_left: 
   st.write("###  新增區") 
   st.button("按鈕放左邊")
-    with st.container(border=True): 
-        st.write(" 標題：開學典禮") 
-        st.write(" 時間：09:00")
+with st.container(border=True): 
+    st.write(" 標題：開學典禮") 
+    st.write(" 時間：09:00")
 
 with col_center: 
   st.write("###  看板區") 
