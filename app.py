4a773c2086e0d8bdd9b1f524eb138400f7facf57
yingnows-coder <yingnows@gmail.com>
2026-05-19 19:20:48 +0800

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,9 @@ col_left, col_center, col_right = st.columns([1, 2, 1.2], gap="large")
 with col_left: 
   st.write("###  新增區") 
   st.button("按鈕放左邊")
+    with st.container(border=True): 
+        st.write(" 標題：開學典禮") 
+        st.write(" 時間：09:00")
 
 with col_center: 
   st.write("###  看板區") 
@@ -18,8 +21,6 @@ with col_right:
   st.write("###  設定區") 
   st.button("控制項放右邊")
 
-with st.container(border=True): 
-    st.write(" 標題：開學典禮") 
-    st.write(" 時間：09:00")
+
 with st.expander("查看進階提醒參數設定"):
-    st.write("這裡是發信伺服器的底層設定...")"
+    st.write("這裡是發信伺服器的底層設定...")
