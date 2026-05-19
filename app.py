bf758d576138c90d595cd8a6f6324440037457bf
yingnows-coder <yingnows@gmail.com>
2026-05-19 19:23:43 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,12 @@ st.set_page_config(page_title="微型 TimeTree", layout="wide")
 with st.sidebar:
     st.write("###  行事曆群組")
     st.radio("選擇群組", ["工作", "家庭", "學校"])
+    @st.dialog("系統公告")
+    def show_alert():
+        st.write("本週作業請確認 requirements.txt 有正確設定！")
+        if st.button("查看公告"): show_alert()
     
-@st.dialog("系統公告")
-def show_alert():
-    st.write("本週作業請確認 requirements.txt 有正確設定！")
-if st.button("查看公告"): show_alert()
+
 
 col_left, col_center, col_right = st.columns([1, 2, 1.2], gap="large")
 
